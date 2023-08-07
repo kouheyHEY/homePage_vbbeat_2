bc798ab3c5c5a003b67e81b39e7adaf2dce0d45f
kouheyHEY <vudtarm@gmail.com>
2023-08-07 23:45:36 +0900

    update work detail

--- hpvbbeat/views/views_work.py
+++ hpvbbeat/views/views_work.py
@@ -3,6 +3,6 @@ from flask import Blueprint, render_template, request
 bp = Blueprint("views_work", __name__, template_folder="templates")
 
 
-@bp.route("/work/<string:category>/<int:id>")
-def work_detail(category, id):
-    return render_template("main/index.html")
+@bp.route("/work/<string:_category>/<int:_id>")
+def work_detail(_category, _id):
+    return render_template("main/work.html", category=_category, id=_id)
